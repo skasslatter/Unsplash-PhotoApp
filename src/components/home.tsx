fbd6fdd3a9ff865fb3341c393c03d5ee4a1b87a8
Sybille <sybille.kasslatter@hotmail.com>
2021-10-10 17:15:12 +0200

    🎨 reindented code

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -3,9 +3,9 @@ import PhotoListComponent from "./photo-list/photo-list";
 
 export const HomeComponent = () => {
   return (
-		<>
-			<h1>Hello, you are seeing a random choice of photos</h1>
-      <PhotoListComponent/>
+    <>
+      <h1>Hello, you are seeing a random choice of photos</h1>
+      <PhotoListComponent />
     </>
   );
 };
