3969c549494b93150d2c9622f3a6ac6416841110
Sybille <sybille.kasslatter@hotmail.com>
2021-10-10 17:14:47 +0200

    ✨ added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import { HomeComponent } from "./components/home";
+import PhotoDetailComponent from "./components/photo-detail/photo-detail";
 require("dotenv").config();
 
 export const App = () => {
   return (
-    <div className="App">
-      <HomeComponent />
-    </div>
+    <>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <HomeComponent />
+          </Route>
+          <Route path="/photos/:id">
+            <PhotoDetailComponent />
+          </Route>
+        </Switch>
+      </div>
+    </>
   );
 };
